99f669ad60f7059cad99b8d76be6f4803756612b
ChrisGongora <chris.gongorag@uanl.edu.mx>
2026-05-14 13:28:20 -0600

    Update dascarga de datos de la API.py
    Archivo definitivo para la descarga de datos de la API de la NASA

--- dascarga.py
+++ dascarga.py
@@ -1,8 +1,7 @@
 import requests
 import json
 
-def informacion_por_fechas(incio, fin):
-    fecha_str = fecha.strftime("%Y-%m-%d")
+def informacion_por_fechas(fecha_str):
     url= "https://api.nasa.gov/neo/rest/v1/feed?start_date="+fecha_str+"&end_date="+fecha_str+"&api_key=qJz6XXC44eUmowIdeDx1pYomWodqRJCWzRa1ynXF"
     info = requests.get(url)
     texto = json.loads(info.text)
