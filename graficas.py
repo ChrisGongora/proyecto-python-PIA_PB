51ebaf267e87b0cdc19b7868f90fb0d66565c9ba
ChrisGongora <chris.gongorag@uanl.edu.mx>
2026-05-14 13:34:23 -0600

    Update graficas.py
    Este archivo le da diseño visual a los datos procesador por el archivo "procedimientos.py"

--- graficas.py
+++ graficas.py
@@ -1,12 +1,21 @@
+import matplotlib.pyplot as plt
+def graficas_cantidad_asteroides(fechas, cantidad_asteroides):
+    plt.style.use('seaborn-v0_8-darkgrid')
+    plt.plot(fechas, cantidad_asteroides, marker = '*', linestyle = ':')
+    plt.title("Cantidad de asteroides detectados")
+    plt.xlabel("Fechas")
+    plt.ylabel("Cantidad")
+    plt.show()
+def graficas_diametro_asteroides(fechas, prom_diametros):
+    plt.plot(fechas, prom_diametros, marker = '*', linestyle = ':')
+    plt.title("Diámetro máximo promedio de los asteroides")
+    plt.xlabel("Fechas")
+    plt.ylabel("Diámetro (metros)")
+    plt.show()
+def graficas_distancia_asteroides(fechas, prom_distancias):
+    plt.plot(fechas, prom_distancias, marker = '*', linestyle = ':')
+    plt.title("Distancia promedio de los asteroides")
+    plt.xlabel("Fechas")
+    plt.ylabel("Distancia (Unidades astronómicas)")
+    plt.show()
 
-def graficas_asteroides(fechas, cantidad_asteroides, prom_diametros, prom_distancias):
-    plt.plot(fechas, cantidad_asteroides, marker = 'P', linestyle = 'dashdot')
-    plt.show()
- 
-    plt.plot(fechas, prom_diametros, marker = 'P', linestyle = 'dashdot')
-    plt.show()
- 
-    plt.plot(fechas, prom_distancias, marker = 'P', linestyle = 'dashdot')
-    plt.show()
-
-    
