7c8978f16b48bfe38a193f1ee0bce16a5fae272f
ChrisGongora <chris.gongorag@uanl.edu.mx>
2026-05-14 13:38:42 -0600

    Update main.py
    Archivo main del proyecto. Aquí se selecciona el periodo de fechas de las que se quiere obtener información, se llaman las funciones de los archivos anteriores y se muestran los resultados de las gráficas del periodo de tiempo seleccionado.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import requests
-import json
 from datetime import datetime, timedelta
 from descarga import informacion_por_fechas
-from procedimientos import procedimientos_asteroides
-from graficas import graficas_asteroides
+from procedimientos import num_de_asteroides, diametro_asteroides, distancia_asteroides, fechas_asteroides
+from graficas import graficas_cantidad_asteroides, graficas_diametro_asteroides, graficas_distancia_asteroides
 
-
-inicio = datetime(2025, 12, 20)
-fin = datetime(2025, 12, 20)
+inicio = datetime(2025, 1, 1)
+fin = datetime(2025, 1, 19)
 fecha = inicio
 
 suma_diametros = 0
@@ -17,6 +14,15 @@ prom_diametros = []
 prom_distancias = []
 fechas = []
 
-def informacion_por_fechas(inicio, fin):
-    def procedimientos_asteroides():
-        def graficas_asteroides():
+while fecha <= fin:
+    fecha_str = fecha.strftime("%Y-%m-%d")
+    texto = informacion_por_fechas(fecha_str)
+    cantidad_asteroides = num_de_asteroides(fecha, fin, texto, cantidad_asteroides)
+    prom_diametros = diametro_asteroides(texto, fecha_str, prom_diametros, suma_diametros)
+    prom_distancias = distancia_asteroides(texto, fecha_str, prom_distancias, suma_distancia)
+    fechas = fechas_asteroides(fechas, fecha_str)
+    fecha += timedelta(days=1)
+    
+graficas_cantidad_asteroides(fechas, cantidad_asteroides)
+graficas_diametro_asteroides(fechas, prom_diametros)
+graficas_distancia_asteroides(fechas, prom_distancias)
